5654d62a8d4d7ef9ad8d8d45ea9c396227074db1
ajithasamuel38 <ajithasamuel38@gamil.com>
2024-01-23 15:44:28 +0530

    Question1

---
+++ Taks1/question1.js
@@ -0,0 +1,10 @@
+const mainheading = document.getElementById('main-heading');
+mainheading.textContent='Fruit World';
+mainheading.style.color='orange';
+const header=document.getElementById('header');
+header.style.backgroundColor='green';
+header.style.borderBottom='solid 3px orange';
+const basketheading=document.getElementById('basket-heading');
+basketheading.style.color='green';
+const thanks=document.getElementById('thanks');
+thanks.innerHTML='<p>Please visit us again</p>';
